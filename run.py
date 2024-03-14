0d700252eebb900960234585dbd637070ba71768
Patrick Hackman <pajohack@gmail.com>
2024-03-14 13:06:33 +0000

    Add logic to check if the user wishes to continue using the program or not

--- run.py
+++ run.py
@@ -10,6 +10,26 @@ def multiply(x,y):
 def divide(x,y):
     return x / y
 
+def check_if_user_is_finished():
+    """
+    Checks if the user wishes to end to program or not.
+    Performs validation on the user input.
+    """
+    ok_to_finish = True
+    user_input_accepted = False
+    
+    while not user_input_accepted:
+        user_input = input('Are you finished using the calculator? (y/n): ')
+        if user_input == 'y':
+            user_input_accepted = True
+        elif user_input == 'n':
+            ok_to_finish = False
+        else:
+            print('Response must be (y/n), please try again.')
+            
+    return ok_to_finish
+
+
 finished = False
 
 while not finished:
@@ -19,6 +39,6 @@ while not finished:
     # select the operation
     print('Result', result)
     print('====================')
-    # determine if the user is finished
+    finished = check_if_user_is_finished()
     
 print('Bye')
