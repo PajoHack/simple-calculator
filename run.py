46f4df10d54c10e79ccbeae667de85efec9544c3
Patrick Hackman <pajohack@gmail.com>
2024-03-14 10:52:28 +0000

    Basic framework added

--- run.py
+++ run.py
@@ -9,3 +9,16 @@ def multiply(x,y):
 
 def divide(x,y):
     return x / y
+
+finished = False
+
+while not finished:
+    result = 0
+    # get operation from user
+    # get the numbers from user
+    # select the operation
+    print('Result', result)
+    print('====================')
+    # determine if the user is finished
+    
+print('Bye')
